4d9d6c125757e511f99f8830307d6b9931e06ba4
Your Name <pavankumar.mn_ee@vvce.ac.in>
2023-11-10 15:18:24 +0530

    s3 name changed

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "pavan-devops888" # Replace with your actual S3 bucket name
+    bucket = "pavan-devops222" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
